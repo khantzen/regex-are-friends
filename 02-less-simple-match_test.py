18f1edc41bf7421e69b469f86fa2413ab0658386
k.hantzen <kevin.hantzen@gmail.com>
2019-12-02 19:57:48 +0100

    no answer

--- 02-less-simple-match_test.py
+++ 02-less-simple-match_test.py
@@ -7,31 +7,31 @@ line_break = '\n'
 class LessSimpleMatchTest(unittest.TestCase):
     # anything between [] can match
     def test_anything_in_array_can_be_matched(self):
-        pattern = r'[a)]'
+        pattern = r'your regex here'
         assert regex.match(pattern, 'a')
         assert regex.match(pattern, ')')
 
     # ^ mark the line begining
     def test_circonflex_mark_a_starting_line(self):
-        pattern = r'^\w'
+        pattern = r'your regex here'
         str_to_match = "a" + line_break + "b" + line_break + "c"
         assert regex.match(pattern, str_to_match)
 
     def test_circonflex_mark_a_starting_line_2(self):
-        pattern = r'^\W\d'
+        pattern = r'your regex here'
         str_to_match = "~8" + line_break + "_9" + line_break + "-6"
         assert regex.match(pattern, str_to_match)
 
     # () capture the matched result
     def test_parenthesis_capture_the_matched_result(self):
-        pattern = r'(abc)'
+        pattern = r'your regex here'
         str_to_search_in = "defdf349,;:abckfifk<àç'"
         search = regex.search(pattern, str_to_search_in)
         assert search[0] == "abc"
 
     # (?<name> ) give a name to the capturing group
     def test_naming_capturing_group(self):
-        pattern = r'(?<number>123)(?<letter>abc)'
+        pattern = r'your regex here'
         str_to_search_in = "dkjg_'_123abcofdo"
         search = regex.search(pattern, str_to_search_in)
 
@@ -39,7 +39,7 @@ class LessSimpleMatchTest(unittest.TestCase):
         assert search['letter'] == 'abc'
 
     def test_group_can_capture_more_than_one_time(self):
-        pattern = r'(?<two_digits>\d\d)'
+        pattern = r'your regex here'
         str_to_search_in = "aaa43hdf64kjdf90"
 
         search = regex.finditer(pattern, str_to_search_in)
@@ -49,7 +49,7 @@ class LessSimpleMatchTest(unittest.TestCase):
 
     # $ mark the end of the line
     def test_dollar_mark_the_end_of_the_line(self):
-        pattern = r'(?<last_number>\d\d)$'
+        pattern = r'your regex here'
 
         first_str_to_search_in = "1234567890"
         second_str_to_search_in = "abde32"
--- 03-conditional-and-loop_test.py
+++ 03-conditional-and-loop_test.py
@@ -5,6 +5,6 @@ import unittest
 # a|b will match a OR b
 class ConditionnalAndLoopTest(unittest.TestCase):
     def test_pipe_work_as_or(self):
-        pattern = r'liskov|noether'
+        pattern = r'your regex here'
         assert regex.match(pattern, 'liskov')
         assert regex.match(pattern, 'noether')
